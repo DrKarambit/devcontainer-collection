f9913877b2b5a978260fbf0b46261aca2baaea91
Antal Kenderesi <43848302+alkenderesi@users.noreply.github.com>
2023-12-02 14:24:06 +0100

    Remove permission fix from setup

--- python/.devcontainer/setup.sh
+++ python/.devcontainer/setup.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-# Fix permissions:
-# https://manpages.ubuntu.com/manpages/jammy/man1/chown.1.html
-# https://manpages.ubuntu.com/manpages/jammy/man1/chmod.1.html
-# https://manpages.ubuntu.com/manpages/jammy/man1/setfacl.1.html
-chown -R `whoami` .
-chmod -R a+rw .
-apt update && apt install -y acl
-setfacl -d -m u::rw .
-setfacl -d -m g::rw .
-setfacl -d -m o::rw .
-
 # Install pip packages:
 # https://pip.pypa.io/en/stable/cli/pip/
 # https://pip.pypa.io/en/stable/cli/pip_install/
